3cfc4efd83d368da28a7863ce1ce27f285582d5d
Davut EREN <davuteren@hotmail.com.tr>
2015-01-16 11:37:11 +0200

    Telefon Defteri Baslangic
    Katmanli Mimari Kullanilması gerekiyor beceremedim

---
+++ TelefonDefteri/TelefonDefteri.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace TelefonDefteri
+{
+    partial class TelefonDefteri
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lstKisiler = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtAra = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.txtTelefon = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.lblTelefon = new System.Windows.Forms.Label();
+            this.lblSoyad = new System.Windows.Forms.Label();
+            this.lblAd = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lstKisiler);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.txtAra);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(318, 481);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Telefon Bilgileri";
+            // 
+            // lstKisiler
+            // 
+            this.lstKisiler.FormattingEnabled = true;
+            this.lstKisiler.ItemHeight = 16;
+            this.lstKisiler.Location = new System.Drawing.Point(6, 98);
+            this.lstKisiler.Name = "lstKisiler";
+            this.lstKisiler.Size = new System.Drawing.Size(306, 372);
+            this.lstKisiler.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::TelefonDefteri.Properties.Resources.find;
+            this.button1.Location = new System.Drawing.Point(255, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 57);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txtAra
+            // 
+            this.txtAra.Location = new System.Drawing.Point(6, 30);
+            this.txtAra.Name = "txtAra";
+            this.txtAra.Size = new System.Drawing.Size(243, 22);
+            this.txtAra.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnKaydet);
+            this.groupBox2.Controls.Add(this.txtTelefon);
+            this.groupBox2.Controls.Add(this.txtSoyad);
+            this.groupBox2.Controls.Add(this.txtAd);
+            this.groupBox2.Controls.Add(this.lblTelefon);
+            this.groupBox2.Controls.Add(this.lblSoyad);
+            this.groupBox2.Controls.Add(this.lblAd);
+            this.groupBox2.Location = new System.Drawing.Point(338, 26);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(294, 468);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Kişisel Bilgiler";
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Image = global::TelefonDefteri.Properties.Resources.save;
+            this.btnKaydet.Location = new System.Drawing.Point(203, 196);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(75, 71);
+            this.btnKaydet.TabIndex = 13;
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // txtTelefon
+            // 
+            this.txtTelefon.Location = new System.Drawing.Point(100, 155);
+            this.txtTelefon.Name = "txtTelefon";
+            this.txtTelefon.Size = new System.Drawing.Size(178, 22);
+            this.txtTelefon.TabIndex = 12;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(100, 121);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(178, 22);
+            this.txtSoyad.TabIndex = 11;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(100, 85);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(178, 22);
+            this.txtAd.TabIndex = 10;
+            // 
+            // lblTelefon
+            // 
+            this.lblTelefon.AutoSize = true;
+            this.lblTelefon.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTelefon.Location = new System.Drawing.Point(27, 155);
+            this.lblTelefon.Name = "lblTelefon";
+            this.lblTelefon.Size = new System.Drawing.Size(73, 17);
+            this.lblTelefon.TabIndex = 9;
+            this.lblTelefon.Text = "Telefon :";
+            // 
+            // lblSoyad
+            // 
+            this.lblSoyad.AutoSize = true;
+            this.lblSoyad.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSoyad.Location = new System.Drawing.Point(27, 121);
+            this.lblSoyad.Name = "lblSoyad";
+            this.lblSoyad.Size = new System.Drawing.Size(67, 17);
+            this.lblSoyad.TabIndex = 8;
+            this.lblSoyad.Text = "Soyadı :";
+            // 
+            // lblAd
+            // 
+            this.lblAd.AutoSize = true;
+            this.lblAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblAd.Location = new System.Drawing.Point(26, 85);
+            this.lblAd.Name = "lblAd";
+            this.lblAd.Size = new System.Drawing.Size(41, 17);
+            this.lblAd.TabIndex = 7;
+            this.lblAd.Text = "Adı :";
+            // 
+            // TelefonDefteri
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 506);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "TelefonDefteri";
+            this.Text = "Telefon Defteri";
+            this.Load += new System.EventHandler(this.TelefonDefteri_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtAra;
+        private System.Windows.Forms.ListBox lstKisiler;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.TextBox txtTelefon;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Label lblTelefon;
+        private System.Windows.Forms.Label lblSoyad;
+        private System.Windows.Forms.Label lblAd;
+    }
+}
+
---
+++ TelefonDefteri/TelefonDefteri.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TelefonDefteri.Models;
+
+namespace TelefonDefteri
+{
+    public partial class TelefonDefteri : Form
+    {
+        public TelefonDefteri()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            TelefonDefteriEntities db = new TelefonDefteriEntities();
+            
+            Kisiler ks = new Kisiler();
+            ks.Ad = txtAd.Text;
+            ks.Soyad = txtSoyad.Text;
+            ks.Telefon = txtTelefon.Text;
+            db.Kisilers.Add(ks);
+            db.SaveChanges();
+
+            txtAd.Text = "";
+            txtSoyad.Text = "";
+            txtTelefon.Text = "";
+            MessageBox.Show("Kayıt Başarıyla Eklendi");
+            Doldur();
+
+        }
+
+        void Doldur()
+        {
+            TelefonDefteriEntities db = new TelefonDefteriEntities();
+
+            lstKisiler.DataSource = db.Kisilers.ToList();
+            //lstKisiler.DisplayMember = "Ad + Soyad";
+ 
+        }
+
+        private void TelefonDefteri_Load(object sender, EventArgs e)
+        {
+            Doldur();
+        }
+    }
+}
